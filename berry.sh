2d92d8d54a5c62aec4ca8a32a3490aa1bcf0e87f
citz <co.jingga@gmail.com>
2022-08-04 01:15:42 +0700

    Update berry.sh

--- berry.sh
+++ berry.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #RUN this after installing base Debian with no GUI and install git
 #Made by @citz_id
-username=$(id -u -n 1000)
+
 ##################################################################################
 ##Install Dekstop Manager
-sudo apt-get -y install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
-sudo dpkg-reconfigure lightdm
+apt-get -y install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+dpkg-reconfigure lightdm
 cp .xinitrc /home/$USER/
 ##################################################################################
 ##Install Berry Window Manager
 #Install Dependencies#
-sudo apt-get -y install libx11-dev libxft-dev libxinerama-dev
+apt-get -y install libx11-dev libxft-dev libxinerama-dev
 
 #Online Install#
 #git clone https://github.com/JLErvin/berry
@@ -22,4 +22,4 @@ sudo apt-get -y install libx11-dev libxft-dev libxinerama-dev
 #cp examples/autostart $HOME/.config/berry/autostart
 
 #Offline Install berry wm#
-sudo dpkg -i ~/home/$username/deb-berry-base/pkg/berry_0.1.11-1_amd64_bullseye.deb
+dpkg -i pkg/berry_0.1.11-1_amd64_bullseye.deb
