d5582e8f306101445af72e5b5bc622e7fbfdfc70
citz <co.jingga@gmail.com>
2022-08-04 01:35:50 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -6,6 +6,7 @@
 ##Change repo Debian to SID 
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp sources.list /etc/apt/sources.list
+cp berry.desktop /usr/share/xsessions
 apt update
 
 chmod +x alacritty.sh berry.sh berry.desktop packages.sh polybar.sh resources.sh .xinitrc pkg/berry_0.1.11-1_amd64_bullseye.deb
