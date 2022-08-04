afed0602e6d956e2ddcbd33c977bfda0d81e47b1
citz <co.jingga@gmail.com>
2022-08-04 22:52:36 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -12,18 +12,19 @@ apt update
 chmod +x alacritty.sh berry.sh berry.desktop packages.sh polybar.sh resources.sh .xinitrc pkg/berry_0.1.11-1_amd64_bullseye.deb
 
 echo "This script is about to run another script."
-./berry.sh
-echo "BerryWm installed."
 ./packages.sh
 echo "Your Packages installed."
-./resources.sh
-echo "Your resources installed."
+./berry.sh
+echo "BerryWm installed."
 #./alacritty.sh
 #echo "Alacritty installed."
 #./polybar.sh
 #echo "Polybar installed."
 #./rofi.sh
 #echo "Polybar installed."
+./resources.sh
+echo "Your resources installed."
+
 
 ##NOTE##
 #uncomment the rest script after BERRYWM run normally. And comment berrysh
