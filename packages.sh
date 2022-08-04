ff3f4bd65080f2bb49e5a75ec4639ad5207f8df0
citz <co.jingga@gmail.com>
2022-08-05 01:32:26 +0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,6 +2,7 @@
 #RUN this after installing base Debian with no GUI and install git
 #Made by @citz_id
 
+
 ##################################################################################
 ##Install terminal emulator
 apt-get install -y kitty
@@ -43,8 +44,43 @@ apt-get install -y nitrogen
 apt-get install -y lxappearance 
 
 ##################################################################################
+##############################Install display manager#############################
+
+##dependencies for ly##
+sudo apt install -y libpam0g-dev libxcb-xkb-dev
+
+##Install Ly Console Display Manager##
+git clone --recurse-submodules https://github.com/nullgemm/ly.git
+cd ly/
+sudo make && sudo make install
+sudo systemctl enable ly
+cd ..
+
+##Lightdm/sddm can be used instead of Ly (more common)##
+##comment out all ly console display if choosing lightdm##
+# sudo apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+# sudo systemctl enable lightdm
+
+
+##XSessions and dwm.desktop##
+# if [[ ! -d /usr/share/xsessions ]]; then
+#     sudo mkdir /usr/share/xsessions
+# fi
+# cat > ./temp << "EOF"
+# [Desktop Entry]
+# Encoding=UTF-8
+# Name=dwm
+# Comment=Dynamic window manager
+# Exec=dwm
+# Icon=dwm
+# Type=XSession
+# EOF
+# sudo cp ./temp /usr/share/xsessions/dwm.desktop;rm ./temp
+
+
+##################################################################################
 ##Install other
-apt-get install -y unzip ranger arandr sxhkd xorg
+apt-get install -y unzip ranger arandr sxhkd fonts-ubuntu
 
 ##################################################################################
 ##Download Nordic Theme
@@ -53,8 +89,9 @@ git clone https://github.com/EliverLara/Nordic.git
 
 ##################################################################################
 ##Install font Fira Code Nerd Font variant needed
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
-unzip FiraCode.zip -d /usr/share/fonts
+#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
+#unzip FiraCode.zip -d /usr/share/fonts
 #wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
 #unzip Meslo.zip -d /usr/share/fonts
+
 fc-cache -vf
