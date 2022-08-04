e32a2d72183244d4140ce3d429efc1a797c84198
citz <co.jingga@gmail.com>
2022-08-05 00:44:26 +0700

    Update resources.sh

--- resources.sh
+++ resources.sh
@@ -3,26 +3,29 @@
 #Made by @citz_id
 
 username=$(id -u -n 1000)
-builddir=$(pwd)
-apt update
+
+##################################################################################
+## Create folder
+cd /home/$username/
+#Create folders in user directory (eg. Documents,Downloads,etc.)#
+mkdir -p Documents Downloads Music Pictures Videos
+mkdir -p .config/bg fonts icons grubthemes
+
 
 ##################################################################################
 ## Clone my resources file (if you have own resources, you can edit this) ##
+cd /home/$username/deb-berry-base/
 git clone https://github.com/citzid/bg.git
 git clone https://github.com/citzid/fonts.git
 git clone https://github.com/citzid/icons.git
 git clone https://github.com/citzid/grubthemes.git
 
+
 ##################################################################################
 ## Copy resources file
-cd $builddir
-#Create folders in user directory (eg. Documents,Downloads,etc.)#
-mkdir -p home/$username/Documents Downloads Music Pictures Videos
-mkdir -p home/$username/.config/bg fonts icons grubthemes
-
 cp -R bg/* /home/$username/.config/bg
-cp -R font/* /home/$username/.config/fonts
-cp -R icon/* /home/$username/.config/icons
+cp -R fonts/* /home/$username/.config/fonts
+cp -R icons/* /home/$username/.config/icons
 cp -R grubthemes/* /home/$username/grubthemes
 
 chown -R $username:$username /home/$username
