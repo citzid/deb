b4a5ae2dfd2364943fc8c50d93fc9a1dc35742c9
citz <co.jingga@gmail.com>
2022-08-04 22:12:34 +0700

    Update berry.sh

--- berry.sh
+++ berry.sh
@@ -6,32 +6,36 @@ username=$(id -u -n 1000)
 
 ##################################################################################
 ##Install Dekstop Manager
-apt-get -y install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+apt-get -y install lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings make gcc
 dpkg-reconfigure lightdm
 
 
+############################Install Berry Window Manager##########################
 ##################################################################################
-##Install Berry Window Manager
-#Install Dependencies#
+
+##################################################################################
+##Install Dependencies##
 apt-get -y install libx11-dev libxft-dev libxinerama-dev
 
 
-#Online Install#
-#git clone https://github.com/JLErvin/berry
-#cd berry
-#make
-#sudo make install
-#mkdir $username/.config/berry
-#cp examples/sxhkdrc $username/.config/berry/sxhkdrc
-#cp examples/autostart $username/.config/berry/autostart
+##################################################################################
+##Online Install##
+git clone https://github.com/JLErvin/berry
+cd berry
+sudo make && sudo make install
+mkdir /home/$username/.config
+mkdir /home/$username/.config/berry
+cp examples/sxhkdrc /home/$username/$username/.config/berry/sxhkdrc
+cp examples/autostart /home/$username/$username/.config/berry/autostart
+cp berry.desktop ~/usr/share/xsessions/
 
-#Offline Install berry wm#
-dpkg -i pkg/berry_0.1.11-1_amd64_bullseye.deb
 
-#setup configuration
-mkdir -p ~/.config
-mkdir -p ~/.config/berry
-cd home/$username
-cp examples/* /home/$username.config/berry/
-cp berry.desktop ~/usr/share/xsessions
-cp .xinitrc /home/$username/
+##################################################################################
+##Offline Install berry wm##
+#dpkg -i pkg/berry_0.1.11-1_amd64_bullseye.deb
+##setup configuration##
+#mkdir -p ~/.config
+#mkdir -p ~/.config/berry
+#cd home/$username
+#cp examples/* /home/$username.config/berry/
+#cp .xinitrc /home/$username/
