e3c89747498a828c6cd8d3c4087feeeebd23c70e
citz <co.jingga@gmail.com>
2022-08-05 07:49:15 +0700

    Update i3install.sh

--- i3install.sh
+++ i3install.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 #RUN this after installing base Debian with no GUI and install git
 #Made by @citz_id
-#SWAY INSTALLER
+#I3WM INSTALLER
 
 ##################################################################################
 ##Change repo Debian to SID 
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp sources.list /etc/apt/sources.list
-apt update && apt upgrade -y
-
 
 ##################################################################################
 ##INSTALL PACKAGES
 chmod +x pkg.sh
 echo "This script is about to run another script."
-./packages.sh
+./pkg.sh
 echo "Your Packages Installed."
-cd ..
+
 
 ##################################################################################
 ##INSTALL DISPLAY MANAGER
@@ -36,33 +34,33 @@ cd ..
 
 ##################################################################################
 ##INSTALL TERMINAL EMULATOR
-cd te
-chmod +x alacritty.sh
-./alacritty.sh
-echo "Alacritty installed."
-cd ..
+#cd te
+#chmod +x alacritty.sh
+#./alacritty.sh
+#echo "Alacritty installed."
+#cd ..
 
 ##################################################################################
 ##INSTALL PANEL/BAR
-cd bar
-chmod +x polybar.sh
-./polybar.sh
-echo "Polybar installed."
-cd ..
+#cd bar
+#chmod +x polybar.sh
+#./polybar.sh
+#echo "Polybar installed."
+#cd ..
 
 ##################################################################################
 ##INSTALL MENU LAUNCHER
-cd mn
-chmod +x rofi.sh
-./rofi.sh
-echo "Rofi installed."
-cd ..
+#cd mn
+#chmod +x rofi.sh
+#./rofi.sh
+#echo "Rofi installed."
+#cd ..
 
 ##################################################################################
 ##INSTALL RESOURCES GIT
-chmod +x resources.sh
-./resources.sh
-echo "Your resources installed."
+#chmod +x resources.sh
+#./resources.sh
+#echo "Your resources installed."
 
 
 ##NOTE##
