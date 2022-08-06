45879543d14090557afaf9482cb1456047666e43
citz <co.jingga@gmail.com>
2022-08-06 23:55:42 +0700

    Update i3-gapsinstall.sh

--- i3-gapsinstall.sh
+++ i3-gapsinstall.sh
@@ -12,56 +12,63 @@ apt update && apt upgrade -y
 ##################################################################################
 ##INSTALL PACKAGES
 chmod +x pkg.sh
-echo "This script is about to run another script."
+echo -e "\e[1;36m Installing pkg.sh \e[0m"
 ./pkg.sh
-echo "Your Packages Installed."
+echo -e "\e[1;42m Your Packages Installed \e[0m"
+
 
 
 ##################################################################################
 ##INSTALL DISPLAY MANAGER
 cd dm
 chmod +x lightdm.sh
+echo -e "\e[1;36m Installing LIGHTDM \e[0m"
 ./lightdm.sh
-echo "Display Manager Installed."
+echo -e "\e[1;42m Display Manager Installed \e[0m"
 cd ..
 
 ##################################################################################
 ##INSTALL WINDOW MANAGER
 cd wm
 chmod +x i3-gaps.sh
+echo -e "\e[1;36m Installing I3-GAPS \e[0m"
 ./i3-gaps.sh
-echo "Window Manager Installed."
+echo -e "\e[1;42m Window Manager Installed \e[0m"
 cd ..
 
 ##################################################################################
 ##INSTALL TERMINAL EMULATOR
 cd te
 chmod +x alacritty.sh
+echo -e "\e[1;36m Installing ALACRITTY \e[0m"
 ./alacritty.sh
-echo "Alacritty installed."
+echo -e "\e[1;42m Alacritty installed \e[0m"
 cd ..
 
 ##################################################################################
 ##INSTALL PANEL/BAR
 cd bar
 chmod +x polybar.sh
+echo -e "\e[1;36m Installing POLYBAR \e[0m"
 ./polybar.sh
-echo "Polybar installed."
+echo -e "\e[1;42m Polybar installed \e[0m"
 cd ..
 
 ##################################################################################
 ##INSTALL MENU LAUNCHER
 cd mn
 chmod +x rofi.sh
+echo -e "\e[1;36m Installing ROFI \e[0m"
 ./rofi.sh
-echo "Rofi installed."
+echo -e "\e[1;42m Rofi installed \e[0m"
 cd ..
 
 ##################################################################################
 ##INSTALL RESOURCES GIT
 chmod +x resources.sh
+echo -e "\e[1;36m Installing RESOURCES\e[0m"
 ./resources.sh
-echo "Your resources installed."
+echo -e "\e[1;42m Your resources installed \e[0m"
 
 
 ##NOTE##
