9018302816123ec7d9e1b99c03191373c17b894f
citz <co.jingga@gmail.com>
2022-08-06 23:38:00 +0700

    Add files via upload

---
+++ i3-gapsinstall.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#RUN this after installing base Debian with no GUI and install git
+#Made by @citz_id
+#I3WM INSTALLER
+
+##################################################################################
+##Change repo Debian to SID 
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cp sources.list /etc/apt/sources.list
+apt update && apt upgrade -y
+
+##################################################################################
+##INSTALL PACKAGES
+chmod +x pkg.sh
+echo "This script is about to run another script."
+./pkg.sh
+echo "Your Packages Installed."
+
+
+##################################################################################
+##INSTALL DISPLAY MANAGER
+cd dm
+chmod +x lightdm.sh
+./lightdm.sh
+echo "Display Manager Installed."
+cd ..
+
+##################################################################################
+##INSTALL WINDOW MANAGER
+cd wm
+chmod +x i3-gaps.sh
+./i3-gaps.sh
+echo "Window Manager Installed."
+cd ..
+
+##################################################################################
+##INSTALL TERMINAL EMULATOR
+cd te
+chmod +x alacritty.sh
+./alacritty.sh
+echo "Alacritty installed."
+cd ..
+
+##################################################################################
+##INSTALL PANEL/BAR
+cd bar
+chmod +x polybar.sh
+./polybar.sh
+echo "Polybar installed."
+cd ..
+
+##################################################################################
+##INSTALL MENU LAUNCHER
+cd mn
+chmod +x rofi.sh
+./rofi.sh
+echo "Rofi installed."
+cd ..
+
+##################################################################################
+##INSTALL RESOURCES GIT
+chmod +x resources.sh
+./resources.sh
+echo "Your resources installed."
+
+
+##NOTE##
+# i comment the rest script because not run normally. you can install manual.
+# you can open and edit _resource.sh_ to use you own resource git.
+
