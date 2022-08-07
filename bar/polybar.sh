aefa5e846287ac4d675d36d50778bb306dd3eadf
citz <co.jingga@gmail.com>
2022-08-08 06:01:17 +0700

    Update polybar.sh

--- bar/polybar.sh
+++ bar/polybar.sh
@@ -8,17 +8,22 @@ username=$(id -u -n 1000)
 ##Install Polybar Panel
 
 #Install dependencies#
-sudo apt-get -y install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev
-sudo apt-get -y install libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev
-sudo apt-get -y install libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen
-sudo apt-get -y install xcb-proto libxcb-xrm-dev i3-wm libasound2-dev
-sudo apt-get -y install libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
+#sudo apt-get -y install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev
+#sudo apt-get -y install libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev
+#sudo apt-get -y install libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen
+#sudo apt-get -y install xcb-proto libxcb-xrm-dev i3-wm libasound2-dev
+#sudo apt-get -y install libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
 
 #Clone the source for Polybar#
-git clone https://github.com/jaagr/polybar.git
+#git clone https://github.com/jaagr/polybar.git
 
 #Build#
-cd polybar && ./build.sh
+#cd polybar && ./build.sh
 
+# Online install #
+apt install -y polybar
+cp launch.sh /home/$username/.config/polybar/config.ini
+chmod +x launch.sh
+cp /usr/share/doc/polybar/config /home/$username/.config/polybar/config.ini
 
 
