8b5c9155c356415f5787fe6309df6ac445aa2106
citz <co.jingga@gmail.com>
2022-08-08 06:03:18 +0700

    Update polybar.sh

--- bar/polybar.sh
+++ bar/polybar.sh
@@ -22,8 +22,8 @@ username=$(id -u -n 1000)
 
 # Online install #
 apt install -y polybar
-cp launch.sh /home/$username/.config/polybar/config.ini
-chmod +x launch.sh
+cp launch.sh /home/$username/.config/polybar/ && chmod +x launch.sh
+
 cp /usr/share/doc/polybar/config /home/$username/.config/polybar/config.ini
 
 
