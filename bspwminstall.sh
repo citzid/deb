ab2c9599c93ec3506145328153b356f1e85df33d
citz <co.jingga@gmail.com>
2022-08-07 17:06:39 -0500

    Update bspwminstall.sh

--- bspwminstall.sh
+++ bspwminstall.sh
@@ -13,56 +13,61 @@ apt update && apt upgrade -y
 ##################################################################################
 ##INSTALL PACKAGES
 chmod +x pkg.sh
-echo "This script is about to run another script."
+echo -e "\e[1;36m Installing pkg.sh \e[0m"
 ./pkg.sh
-echo "Your Packages Installed."
+echo -e "\e[1;42m Your Packages Installed \e[0m"
 
 ##################################################################################
 ##INSTALL DISPLAY MANAGER
 cd dm
-chmod +x ly.sh
-./ly.sh
-echo "Display Manager Installed."
+chmod +x sddm.sh
+echo -e "\e[1;36m Installing SDDM \e[0m"
+./sddm.sh
+echo -e "\e[1;42m Display Manager Installed \e[0m"
 cd ..
 
 ##################################################################################
 ##INSTALL WINDOW MANAGER
 cd wm
 chmod +x bspwm.sh
+echo -e "\e[1;36m Installing BSPWM \e[0m"
 ./bspwm.sh
-echo "Window Manager Installed."
+echo -e "\e[1;42m Window Manager Installed \e[0m"
 cd ..
 
 ##################################################################################
 ##INSTALL TERMINAL EMULATOR
-#cd te
-#chmod +x alacritty.sh
-#./alacritty.sh
-#echo "Alacritty installed."
-#cd ..
+cd te
+chmod +x alacritty.sh
+cho -e "\e[1;36m Installing ALACRITTY \e[0m"
+./alacritty.sh
+echo -e "\e[1;42m Alacritty installed \e[0m"
+cd ..
 
 ##################################################################################
 ##INSTALL PANEL/BAR
-#cd bar
-#chmod +x polybar.sh
-#./polybar.sh
-#echo "Polybar installed."
-#cd ..
+cd bar
+chmod +x polybar.sh
+echo -e "\e[1;36m Installing POLYBAR \e[0m"
+./polybar.sh
+echo -e "\e[1;42m Polybar installed \e[0m"
+cd ..
 
 ##################################################################################
 ##INSTALL MENU LAUNCHER
-#cd mn
-#chmod +x rofi.sh
-#./rofi.sh
-#echo "Rofi installed."
-#cd ..
+cd mn
+chmod +x rofi.sh
+echo -e "\e[1;36m Installing ROFI \e[0m"
+./rofi.sh
+echo -e "\e[1;42m Rofi installed \e[0m"
+cd ..
 
 ##################################################################################
 ##INSTALL RESOURCES GIT
-#chmod +x resources.sh
-#./resources.sh
-#echo "Your resources installed."
-
+chmod +x resources.sh
+echo -e "\e[1;36m Installing RESOURCES\e[0m"
+./resources.sh
+echo -e "\e[1;42m Your resources installed \e[0m"
 
 ##NOTE##
 # i comment the rest script because not run normally. you can install manual.
