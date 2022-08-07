ad34c906038dbaa32fe0415be7fa1e4f8ad3c9d8
citz <co.jingga@gmail.com>
2022-08-08 03:08:16 +0700

    Update bspwm.sh

--- wm/bspwm.sh
+++ wm/bspwm.sh
@@ -4,36 +4,46 @@
 username=$(id -u -n 1000)
 
 
-############################Install BSP Window Manager##########################
+############################Install BSPWM Window Manager##########################
 ##################################################################################
 
 ##################################################################################
 ##Install Dependencies##
-apt-get -y install libx11-dev libxft-dev libxinerama-dev
-
+sudo apt install -y gcc make xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev
+sudo apt install -y libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev
+sudo apt install -y libasound2-dev libxcb-xtest0-dev xorg xorg-server xorg-xinit
 
 ##################################################################################
-##Online Install##
-sudo apt install -y bspwm sxhkd polybar xorg
+## Install bspwm sxhkd ##
+git clone https://github.com/baskerville/bspwm.git
+git clone https://github.com/baskerville/sxhkd.git
+git clone https://github.com/baskerville/xdo
+
+cd sxhkd && make && sudo make install
+cd ../bspwm && make && sudo make install
+cd ../xdo && make && sudo make install
+
+sudo apt install -y bspwm sxhkd
 sudo apt autoremove -y lemonbar
 
-
-
 ##################################################################################
-##Copy configuration##
-cd /home/$username
-mkdir -p .config
-cd /home/$username/.config
-mkdir -p bspwm sxhkd polybar
+## Configuration ##
+mkdir -p /home/$username/.config/{bspwm,sxhkd}
 
+## Copy Template configuration ##
 cd /home/$username/deb/wm
-cp /usr/share/doc/bspwm/examples/bspwmrc /home/$username/.config/bspwm/
-cp /usr/share/doc/bspwm/examples/sxhkdrc /home/$username/.config/sxhkd/
-cp /usr/share/doc/polybar/examples/config.ini /home/$username/.config/polybar/
+cp bspwmrc /home/$username/.config/bspwm/
+cp sxhkdrc /home/$username/.config/sxhkd/
 cp .xinitrc /home/$username/
 
+## move original config example for backup ##
+cp /usr/share/doc/bspwm/examples/bspwmrc /home/$username/.config/bspwm/bspwmr.bak
+cp /usr/share/doc/bspwm/examples/sxhkdrc /home/$username/.config/sxhkd/sxhkdrc.bak
+
+##################################################################################
 chmod +x /home/$username/.config/bspwm/bspwmrc
 chmod +x /home/$username/.config/sxhkd/sxhkdrc
 chmod +x /home/$username/.xinitrc
-##################################################################################
+
+######################################EOF#########################################
 
