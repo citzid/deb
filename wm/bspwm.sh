7e88b924fdb766ab17eabaf1fdd9ca10c952192a
citz <co.jingga@gmail.com>
2022-08-09 16:02:03 -0500

    Update bspwm.sh

--- wm/bspwm.sh
+++ wm/bspwm.sh
@@ -46,5 +46,20 @@ chmod +x /home/$username/.config/bspwm/bspwmrc
 chmod +x /home/$username/.config/sxhkd/sxhkdrc
 chmod +x /home/$username/.xinitrc
 
+##################################################################################
+## Picom compositor install
+cd /home/$username/deb/
+git clone https://github.com/yshui/picom.git && cd picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
+cd /home/$username/deb/wm
+mkdir -p /home/$username/.config/picom
+cp -R picom.conf cd /home/$username/.config/picom/
+
+
 ######################################EOF#########################################
+
 
