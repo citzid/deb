cd22f4425cf3812aa991fd1a4a611dff75614d76
citz <co.jingga@gmail.com>
2022-08-09 16:16:27 -0500

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -40,6 +40,16 @@ apt-get install -y geany
 #apt-get install -y vim
 #apt-get install -y neovim
 
+## VS-CODE
+#sudo apt-get install wget gpg
+#wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+#sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+#sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+#rm -f packages.microsoft.gpg
+#sudo apt install apt-transport-https
+#sudo apt update
+#sudo apt install code # or code-insiders
+
 
 
 ##################################################################################
