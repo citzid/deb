95757f9ad2ade1dab688fd6b622f8c1d54f6d809
citz <co.jingga@gmail.com>
2022-08-12 02:57:40 +0700

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -19,11 +19,11 @@ xhost +local:
 #apt-get install -y chromium
 
 ## BRAVE BROWSER INSTALL
-sudo apt install apt-transport-https curl
+sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install -y brave-browser
 
 
 ##################################################################################
@@ -39,9 +39,9 @@ apt-get install -y thunar
 ##################################################################################
 ##Install Launcher
 
-#sudo apt install rofi
-#sudo apt install wofi
-sudo apt install dmenu
+#sudo apt install -y rofi
+#sudo apt install -y wofi
+sudo apt install -y dmenu
 
 
 ##################################################################################
