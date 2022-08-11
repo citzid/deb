74cb886c8a087db7948b94a3897c1920094ba9c8
citz <co.jingga@gmail.com>
2022-08-12 03:07:41 +0700

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -19,11 +19,11 @@ xhost +local:
 #apt-get install -y chromium
 
 ## BRAVE BROWSER INSTALL
-sudo apt install apt-transport-https curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt install software-properties-common apt-transport-https wget ca-certificates gnupg2 -y
+wget -O- https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/brave-browser-archive-keyring.gpg
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install -y brave-browser
+sudo apt install brave-browser -y
 
 
 ##################################################################################
