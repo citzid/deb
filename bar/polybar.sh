8d97e54405b81d089b09a3dc935ab241dfb2d80d
citz <co.jingga@gmail.com>
2022-08-11 22:04:10 -0500

    Update polybar.sh

--- bar/polybar.sh
+++ bar/polybar.sh
@@ -14,6 +14,9 @@ username=$(id -u -n 1000)
 #sudo apt-get -y install xcb-proto libxcb-xrm-dev i3-wm libasound2-dev
 #sudo apt-get -y install libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
 
+apt install -y build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+apt install -y libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+
 
 #Clone the source for Polybar#
 #git clone https://github.com/jaagr/polybar.git
