1b07831d88ba81298f0a9177e83a2085c7df00f0
citz <co.jingga@gmail.com>
2022-08-14 06:19:45 +0700

    Update alacritty.sh

--- te/alacritty.sh
+++ te/alacritty.sh
@@ -5,51 +5,44 @@
 ##########################################################################
 ##Install Alacritty terminal emulator
 
-#If you're just interested in the Alacritty binary and you don't need the terminfo file, desktop entry, manual page or shell completions, you can install it directly through cargo:
-
-#apt install -y cargo
-#cargo install alacritty
+cargo install alacritty
 
 
 ###################################FULLINSTALL###################################
-
 #install
-apt-get install -y curl cargo
-git clone https://github.com/alacritty/alacritty.git
-cd alacritty
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup override set stable
-rustup update stable
-
-#dependencies
-apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 xfce4-settings
+#apt-get install -y curl cargo
+#git clone https://github.com/alacritty/alacritty.git
+#cd alacritty
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#rustup override set stable
+#rustup update stable
 
 ###Building###
-cargo build --release
+#cargo build --release
 
 #Terminfo
-sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+#sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 
 #Desktop Entry
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install extra/linux/Alacritty.desktop
-sudo update-desktop-database
+#sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+#sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+#sudo desktop-file-install extra/linux/Alacritty.desktop
+#sudo update-desktop-database
 
 #If you are having problems with Alacritty's logo, you can replace it with prerendered PNGs and simplified SVGs available in the extra/logo/compat directory.
 
 #Manual Page
-sudo mkdir -p /usr/local/share/man/man1
-gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+#sudo mkdir -p /usr/local/share/man/man1
+#gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+#gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
 
 #Shell completions
 ##########################################################################
 #ZSH
-apt-get install -y zsh
-mkdir -p ${ZDOTDIR:-~}/.zsh_functions
-echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
-cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+#apt-get install -y zsh
+#mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+#echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
+#cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
 
 ##########################################################################
 #BASH
@@ -71,6 +64,5 @@ cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
 #mkdir -p $fish_complete_path[1]
 #cp extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
 
-#this line to fix gprated wont run
-xhost +local:
+
 
